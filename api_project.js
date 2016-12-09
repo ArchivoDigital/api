5afc2732d537e9c254b264b21cb0c632dcd65544
lgguzman <ing.luis.guzman.reyes@gmail.com>
2016-12-09 11:45:38 -0500

    first commit

--- api_project.js
+++ api_project.js
@@ -1,1 +1,13 @@
-define({  "name": "Archivo.Digital",  "version": "1.0.20161122",  "description": "Documentación de la API",  "apidoc": "0.2.0",  "sampleUrl": false,  "generator": {    "name": "apidoc",    "time": "2016-11-22T17:13:14.476Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
+define({
+  "name": "Archivo.Digital",
+  "version": "1.0.20161122",
+  "description": "Documentación de la API",
+  "apidoc": "0.2.0",
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-12-09T15:51:54.212Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
